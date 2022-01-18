7f17ddfa65c2c6525bd26b9fb420d29eb9f3afb0
Kyle Cotton <kylecottonkc@gmail.com>
2022-01-18 16:19:21 +0000

    Updated the automatically generated python library

--- api-python/proto/index_updater_pb2.py
+++ api-python/proto/index_updater_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x19proto/index_updater.proto\x12\x0cindex_update\"\x98\x01\n\x12IndexUpdateRequest\x12\x0e\n\x06\x64omain\x18\x01 \x01(\t\x12\x14\n\x0c\x61rticleTitle\x18\x02 \x01(\t\x12\x17\n\x0flastUpdatedDate\x18\x03 \x01(\t\x12\x18\n\x10preprocessedText\x18\x04 \x03(\t\x12\x11\n\ttextLinks\x18\x05 \x03(\t\x12\x16\n\x0etextCategories\x18\x06 \x03(\t\"\x15\n\x13IndexUpdateResponse2e\n\x0bIndexUpdate\x12V\n\rupdateArticle\x12 .index_update.IndexUpdateRequest\x1a!.index_update.IndexUpdateResponse\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x19proto/index_updater.proto\x12\rindex_updater\"\x98\x01\n\x12IndexUpdateRequest\x12\x0e\n\x06\x64omain\x18\x01 \x01(\t\x12\x14\n\x0c\x61rticleTitle\x18\x02 \x01(\t\x12\x17\n\x0flastUpdatedDate\x18\x03 \x01(\t\x12\x18\n\x10preprocessedText\x18\x04 \x03(\t\x12\x11\n\ttextLinks\x18\x05 \x03(\t\x12\x16\n\x0etextCategories\x18\x06 \x03(\t\"\x15\n\x13IndexUpdateResponse2g\n\x0bIndexUpdate\x12X\n\rupdateArticle\x12!.index_updater.IndexUpdateRequest\x1a\".index_updater.IndexUpdateResponse\"\x00\x62\x06proto3')
 
 
 
@@ -23,14 +23,14 @@ _INDEXUPDATERESPONSE = DESCRIPTOR.message_types_by_name['IndexUpdateResponse']
 IndexUpdateRequest = _reflection.GeneratedProtocolMessageType('IndexUpdateRequest', (_message.Message,), {
   'DESCRIPTOR' : _INDEXUPDATEREQUEST,
   '__module__' : 'proto.index_updater_pb2'
-  # @@protoc_insertion_point(class_scope:index_update.IndexUpdateRequest)
+  # @@protoc_insertion_point(class_scope:index_updater.IndexUpdateRequest)
   })
 _sym_db.RegisterMessage(IndexUpdateRequest)
 
 IndexUpdateResponse = _reflection.GeneratedProtocolMessageType('IndexUpdateResponse', (_message.Message,), {
   'DESCRIPTOR' : _INDEXUPDATERESPONSE,
   '__module__' : 'proto.index_updater_pb2'
-  # @@protoc_insertion_point(class_scope:index_update.IndexUpdateResponse)
+  # @@protoc_insertion_point(class_scope:index_updater.IndexUpdateResponse)
   })
 _sym_db.RegisterMessage(IndexUpdateResponse)
 
@@ -38,10 +38,10 @@ _INDEXUPDATE = DESCRIPTOR.services_by_name['IndexUpdate']
 if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
-  _INDEXUPDATEREQUEST._serialized_start=44
-  _INDEXUPDATEREQUEST._serialized_end=196
-  _INDEXUPDATERESPONSE._serialized_start=198
-  _INDEXUPDATERESPONSE._serialized_end=219
-  _INDEXUPDATE._serialized_start=221
-  _INDEXUPDATE._serialized_end=322
+  _INDEXUPDATEREQUEST._serialized_start=45
+  _INDEXUPDATEREQUEST._serialized_end=197
+  _INDEXUPDATERESPONSE._serialized_start=199
+  _INDEXUPDATERESPONSE._serialized_end=220
+  _INDEXUPDATE._serialized_start=222
+  _INDEXUPDATE._serialized_end=325
 # @@protoc_insertion_point(module_scope)
